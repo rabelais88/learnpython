5a02f863e25f542ce5c6ea7919d4c595e785b82f
rabelais88 <sungryeolp@gmail.com>
2018-04-04 06:16:01 +0900

    ddd

---
+++ stringtest.py
@@ -0,0 +1,8 @@
+s1 = "abc"
+s1 = s1 * 3
+print(s1)
+print(s1[0])
+#text slice is same as list
+addr = "http://www.helloworld.com".split("/",maxsplit=1) # split only once!
+print(addr)
+print("/".join(addr))
